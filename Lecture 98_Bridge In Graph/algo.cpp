9e141e664b94cc769abfc916da54fbea9b44d99c
94prathampatil <07prathampatil06@gmail.com>
2025-06-10 17:33:14 +0530

    lecture 99_Articualtion points in Graph added

---
+++ Lecture 98_Bridge In Graph/algo.cpp
@@ -0,0 +1,26 @@
+// What is Bridge
+// A edge if we remove it from the graph it divides the graph into two different components
+
+#include<bits/stdc++.h>
+#define ll long long
+#define llv long long v
+using namespace std;
+
+class Solution{
+
+    public:
+        void solve(){
+            ios_base::sync_with_stdio(0);
+            cin.tie(0);
+            cout.tie(0);
+            
+            
+        }
+};
+
+int main()
+{
+    Solution sl;
+    sl.solve();
+    return 0;
+}
